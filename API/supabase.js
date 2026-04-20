2b1d6e0d3283e42da5a50756ddaad0a5f00b4730
bkornblith <ben.harris.kornblith@gmail.com>
2026-04-20 11:14:51 -0400

    fix

---
+++ API/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  process.env.https://fqpjhuztptppyplpsgkh.supabase.co,
+  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxcGpodXp0cHRwcHlwbHBzZ2toIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NjYzOTc5MywiZXhwIjoyMDkyMjE1NzkzfQ.JMcsDK8WEacnWfBjj-rgSc9ipojuNVJv7J6PkRe4C8c
+
+)
